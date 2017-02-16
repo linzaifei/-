ed8df05c2dc1bc11f968a2ddbf9da54882b6f749
林再飞 <linzaifei@xinshiyun.com>
2017-02-16 10:42:55 +0800

    Initial Commit

---
+++ 转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转场动画
+//
+//  Created by xsy on 2017/2/16.
+//  Copyright © 2017年 林再飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 转场动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  转场动画
+//
+//  Created by xsy on 2017/2/16.
+//  Copyright © 2017年 林再飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
